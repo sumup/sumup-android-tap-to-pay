71a8023053680c872fecacb8633b75465da59b14
murzagalin <azamat.murzagalin@sumup.com>
2025-10-21 13:12:58 +0200

    refer public docs

--- tap-to-pay-sampleapp/build.gradle.kts
+++ tap-to-pay-sampleapp/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 }
 
 dependencies {
-    implementation("com.sumup.tap-to-pay:utopia-sdk:1.0.0")
+    implementation("com.sumup.tap-to-pay:utopia-sdk:1.0.1")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.8.0")
